024c215a7c111c9637a741d512bee465c147edd9
LoomyGames <39547531+LoomyGames@users.noreply.github.com>
2022-05-23 23:01:54 +0100

    Update ResetWall.cs

--- ProjectFiles/Assets/Scripts/ResetWall.cs
+++ ProjectFiles/Assets/Scripts/ResetWall.cs
@@ -12,14 +12,14 @@ public class ResetWall : MonoBehaviour
         CC = GetComponent<CharacterController>();
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other) // if the player falls out of the map (in the lobby) reset its position back to the surface 
     {
         if (other.CompareTag("WallReset"))
         {
-            CC.enabled = false;
+            CC.enabled = false; //disable the controller for a bit
             Debug.Log("Reset player location");
-            gameObject.transform.position = resetLocation.transform.position;
-            CC.enabled = true;
+            gameObject.transform.position = resetLocation.transform.position; //change the location
+            CC.enabled = true; //re-enable the controller
         }
     }
 }
