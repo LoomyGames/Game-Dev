06a3094ba4e332c3c7d078e3799e995221d14c6f
LoomyGames <39547531+LoomyGames@users.noreply.github.com>
2022-05-23 23:15:46 +0100

    Update PlaneShooting.cs

--- ProjectFiles/Assets/Scripts/PlaneShooting.cs
+++ ProjectFiles/Assets/Scripts/PlaneShooting.cs
@@ -5,28 +5,23 @@ using UnityEngine;
 public class PlaneShooting : MonoBehaviour
 {
     [SerializeField]
-    private int damage = 20;
+    private int damage = 20;//base bullet damage
     [SerializeField]
-    ParticleSystem muzzleFlash;
+    ParticleSystem muzzleFlash;//muzzleFlash particle system
 
 
-    public Transform shootingPoint;
-    public GameObject projectile;
-    public float launchVelocity = 700f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform shootingPoint; //starting point where bullets spawn
+    public GameObject projectile; //bullet prefab
+    public float launchVelocity = 700f; //bullet initial velocity
 
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1")) //if the left mouse button is pressed (the plane gun fires semi-automatic)
         {
-            muzzleFlash.Play();
-            GameObject ball = Instantiate(projectile, shootingPoint.transform.position, shootingPoint.transform.rotation);
-            ball.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, 0, launchVelocity));
-            Destroy(ball, 4);
+            muzzleFlash.Play(); //play the muzzle flash
+            GameObject ball = Instantiate(projectile, shootingPoint.transform.position, shootingPoint.transform.rotation); //instantiate the bullet at the shooting point
+            ball.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, 0, launchVelocity)); //add force to the rigidbody, to push the bullet forward
+            Destroy(ball, 4); // if the bullet does not touch anything in 4 seconds it will be automatically destroyed for optimization purposes
         }
     }
     
