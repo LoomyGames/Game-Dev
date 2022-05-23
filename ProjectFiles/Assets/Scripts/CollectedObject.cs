32128114dce7468ee502a68008fbdf8b16667632
LoomyGames <39547531+LoomyGames@users.noreply.github.com>
2022-05-23 22:45:50 +0100

    Update CollectedObject.cs

--- ProjectFiles/Assets/Scripts/CollectedObject.cs
+++ ProjectFiles/Assets/Scripts/CollectedObject.cs
@@ -7,37 +7,31 @@ public class CollectedObject : MonoBehaviour
 {
     [SerializeField]
     public string objName = "";
-    enum UpgradeName
+    enum UpgradeName //an enum holding what type of upgrade this relic gives
     {
         Airport,
         Runway,
         Warehouse
     };
     [SerializeField]
-    UpgradeName upgrade;
-    InventoryManager IM;
+    UpgradeName upgrade; //the actual reference to the upgrade
+    InventoryManager IM; //reference to the inventory manager controller
     Text pickText;
     // Start is called before the first frame update
     void Start()
     {
-        IM = GameObject.FindWithTag("Inventory Manager").GetComponent<InventoryManager>();
+        IM = GameObject.FindWithTag("Inventory Manager").GetComponent<InventoryManager>(); //initializing the variables
         pickText = GameObject.FindWithTag("DialogueText").GetComponent<Text>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other) //when the player enters the trigger of this relic
     {
         if(other.CompareTag("Player"))
         {
-            pickText.text = "Collected " + objName;
-            pickText.GetComponent<ResetText>().ResetWriting();
-            IM.IncreaseBuildingLevel(upgrade.ToString());
-            Destroy(gameObject);
+            pickText.text = "Collected " + objName; //state that it has been collected
+            pickText.GetComponent<ResetText>().ResetWriting(); //after a couple of seconds hide the collection text
+            IM.IncreaseBuildingLevel(upgrade.ToString()); // increase the level of the building this upgrade was for 
+            Destroy(gameObject); // destroy the gameobject
         }
     }
 }
