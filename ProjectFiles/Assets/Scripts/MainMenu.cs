80be9aa0886d7a32fe92b2b0042e3d7d7aab6c44
LoomyGames <39547531+LoomyGames@users.noreply.github.com>
2022-05-23 23:22:31 +0100

    Update MainMenu.cs

--- ProjectFiles/Assets/Scripts/MainMenu.cs
+++ ProjectFiles/Assets/Scripts/MainMenu.cs
@@ -3,51 +3,51 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
-    public GameObject QuitMenu;
+    public GameObject QuitMenu; //reference all of the UI elements
     public GameObject OptionsMenu;
     public Button StartGame;
     public Button Options;
     public Button Quit;
 
     // Start is called before the first frame update
-    public void Start()
+    public void Start() //disable the options (controls) and quit sub-menus
     {
         QuitMenu.SetActive(false);
         OptionsMenu.SetActive(false);
     }
 
-    public void StartClick()
+    public void StartClick() //start the game by going to the lobby scene if start is pressed
     {
         SceneManager.LoadScene("GameLobby");
     }
-    public void OptionsClick()
+    public void OptionsClick() //if the user presses options (now controls) enable that submenu and disable the other buttons
     {
         OptionsMenu.SetActive(true);
         DisableButtons();
     }
-    public void QuitClick()
+    public void QuitClick() //if the user presses quit, enable that submenu for confirmation and disable the other buttons
     {
         QuitMenu.SetActive(true);
         DisableButtons();
     }
-    public void YesClick()
+    public void YesClick() // quit the game if yes is selected on the submenu
     {
         Application.Quit();
     }
-    public void NoClick()
+    public void NoClick() // return to the original state if no is slected on the submenu
     {
         QuitMenu.SetActive(false);
         OptionsMenu.SetActive(false);
         EnableButtons();
     }
 
-    public void DisableButtons()
+    public void DisableButtons() // method for disabling all of the buttons
     {
         StartGame.enabled = false;
         Options.enabled = false;
         Quit.enabled = false;
     }
-    public void EnableButtons()
+    public void EnableButtons() //method for re-enabling the buttons
     {
         StartGame.enabled = true;
         Options.enabled = true;
