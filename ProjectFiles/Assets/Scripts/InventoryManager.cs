810f9e32c1708dfab77ea8608bf2e6991a185770
LoomyGames <39547531+LoomyGames@users.noreply.github.com>
2022-05-23 23:40:38 +0100

    Update InventoryManager.cs

--- ProjectFiles/Assets/Scripts/InventoryManager.cs
+++ ProjectFiles/Assets/Scripts/InventoryManager.cs
@@ -4,25 +4,22 @@ using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
 {
-    [SerializeField]
-    private int currency = 0;
-
-    private GameObject Airport;
+    private GameObject Airport; //the parents of each group of upgrades
     private GameObject Runway;
     private GameObject Warehouse;
 
-    int airportLevel = -1;
+    int airportLevel = -1; //the levels of the groups (starting with -1 for not even being built)
     int runwayLevel = -1;
     int warehouseLevel = -1;
 
     // Start is called before the first frame update
     void Start()
     {
-        FindBuildings();
-        DontDestroyOnLoad(gameObject);
+        FindBuildings(); //upon entering the scene, find the buildings and don't destroy the object after leaving the scene
+        DontDestroyOnLoad(gameObject); //this object follows the player throughout the levels and updates constantly
     }
 
-    public void FindBuildings()
+    public void FindBuildings() //find the upgrade groups' parents
     {
         Airport = GameObject.FindWithTag("Airport");
         Runway = GameObject.FindWithTag("Runway");
@@ -30,17 +27,12 @@ public class InventoryManager : MonoBehaviour
 
     }
 
-    public int GetCurrency()
+    public void UpgradeBuilding(string name) //upgrade the building based on the collected relic, method called when switching the scene from a level to the lobby
     {
-        return currency;
-    }
-
-    public void UpgradeBuilding(string name)
-    {
-        if (name == "Airport")
+        if (name == "Airport") //if the building name is Airport, find all of the children groups and enable them based on how many levels the manager has
         {
             int i = 0;
-            foreach (Transform child in Airport.transform)
+            foreach (Transform child in Airport.transform) //this sets the groups active on each load into the lobby based on the current group level
             {
                 if (i <= airportLevel)
                 {
@@ -49,7 +41,7 @@ public class InventoryManager : MonoBehaviour
                 }
             }
         }
-        else if (name == "Warehouse")
+        else if (name == "Warehouse") //this is the same as above, only for the Warehouse group
         {
             int i = 0;
             foreach (Transform child in Warehouse.transform)
@@ -61,7 +53,7 @@ public class InventoryManager : MonoBehaviour
                 }
             }
         }
-        else if (name == "Runway")
+        else if (name == "Runway") //this is the same as above, only for the Runway group
         {
             int i = 0;
             foreach (Transform child in Runway.transform)
@@ -75,16 +67,16 @@ public class InventoryManager : MonoBehaviour
         }
     }
 
-    public void IncreaseBuildingLevel(string name)
+    public void IncreaseBuildingLevel(string name) //this method is called when picking up a relic and increases the level of the specified group
     {
-        if (name == "Airport")
+        if (name == "Airport") //all of the statements here work the same, but for the 3 different groups
         {
             if (airportLevel < 3)
             {
                 airportLevel++;
             }
             else
-            {
+            { //the max level of each group is 3, but this can be easily scaled up indefinitely
                 airportLevel = 3;
                 Debug.Log("Airport at max level");
             }
@@ -115,7 +107,7 @@ public class InventoryManager : MonoBehaviour
         }
     }
 
-    public void GenerateUpgrades()
+    public void GenerateUpgrades() // generate the upgrades, method called when entering the lobby again after leaving a level
     {
         UpgradeBuilding("Airport");
         UpgradeBuilding("Warehouse");
