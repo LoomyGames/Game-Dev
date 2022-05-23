ecf973c73e9b67ef3b403f41c3177ce4291390b1
LoomyGames <39547531+LoomyGames@users.noreply.github.com>
2022-05-23 23:00:30 +0100

    Update EnterPlane.cs

--- ProjectFiles/Assets/Scripts/EnterPlane.cs
+++ ProjectFiles/Assets/Scripts/EnterPlane.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class EnterPlane : MonoBehaviour
 {
-    GameObject player;
+    GameObject player; // player and plane related variables needed for switching controllers
     public GameObject planeCam;
     public Transform animCamPos;
     public bool inPlane = false;
@@ -18,7 +18,7 @@ public class EnterPlane : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindWithTag("Player");
+        player = GameObject.FindWithTag("Player"); // find variables
         planeCam.SetActive(false);
         interactText = GameObject.FindWithTag("DialogueText").GetComponent<Text>();
     }
@@ -27,23 +27,23 @@ public class EnterPlane : MonoBehaviour
     void Update()
     {
 
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E)) //if the player presses E
         {
-            if (inPlane && collectedRelic)
+            if (inPlane && collectedRelic) // only if the player is in the plane and has collected the relic can they leave 
             {
                 Debug.Log("Exited Plane");
-                StartCoroutine(WaitForAnim());
-                GetComponent<PlaneController>().enabled = false;
+                StartCoroutine(WaitForAnim()); //another coroutine for waiting to enable the player only after the animation finished
+                GetComponent<PlaneController>().enabled = false; //disable the plane systems
                 GetComponent<PlaneShooting>().enabled = false;
                 planeCam.transform.position = animCamPos.position;
                 planeCam.transform.rotation = animCamPos.rotation;
                 inPlane = false;
-                planeClone.SetActive(true);
+                planeClone.SetActive(true); //enable the clone mesh of the plane that actually plays the animation
                 //planeClone.GetComponent<Animator>().Play("PlaneLand");
-                planeMesh.SetActive(false);
-            } else if(!inPlane && inRange)
+                planeMesh.SetActive(false); //disable the mesh of the actual flown plane
+            } else if(!inPlane && inRange) // otherwise, the player can enter the plane if in range
             {
-                interactText.text = "";
+                interactText.text = ""; //the exact opposite, enable the plane systems and disable the player ones
                 Debug.Log("Entered plane");
                 player.SetActive(false);
                 GetComponent<PlaneController>().enabled = true;
@@ -55,7 +55,7 @@ public class EnterPlane : MonoBehaviour
         }
     }
 
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerStay(Collider other) //while in the trigger range of the plane, the player can enter if they press E and a prompt appears
     {
         if (other.gameObject == player)
         {
@@ -66,14 +66,14 @@ public class EnterPlane : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject == player)
+        if (other.gameObject == player) // as soon as the player leaves this range, the prompt disappears and the plane can not be entered
         {
             inRange = false;
             interactText.text = "";
         }
     }
 
-    IEnumerator WaitForAnim()
+    IEnumerator WaitForAnim()//the coroutine that controls the wait time for the landing animation
     {
         yield return new WaitForSeconds(3);
         player.SetActive(true);
