23f1a2d93fefb5bcfd9277a93a5b35135de3b800
LoomyGames <39547531+LoomyGames@users.noreply.github.com>
2022-05-23 23:13:15 +0100

    Update ReleaseRelic.cs

--- ProjectFiles/Assets/Scripts/ReleaseRelic.cs
+++ ProjectFiles/Assets/Scripts/ReleaseRelic.cs
@@ -6,9 +6,9 @@ using UnityEngine.UI;
 public class ReleaseRelic : MonoBehaviour
 {
     [SerializeField]
-    int buildingsDestroyed = 0;
+    int buildingsDestroyed = 0; //the number of buildings currently destroyed
     [SerializeField]
-    int buildingsRequired = 0;
+    int buildingsRequired = 0; //the number of destroyed buildings required (in the demo it's 5, but that can be adjusted or even randomized in the future)
     public GameObject relic;
     EnterPlane plane;
     Text finishText;
@@ -16,32 +16,26 @@ public class ReleaseRelic : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        plane = GameObject.FindWithTag("Plane").GetComponent<EnterPlane>();
+        plane = GameObject.FindWithTag("Plane").GetComponent<EnterPlane>();//get the plane controller (for setting the collectedRelic boolean upon completion
         finishText = GameObject.Find("DialogueText").GetComponent<Text>();
-        finishText.text = "Quick, get in the plane and destroy the buildings to collect the relic!";
-        StartCoroutine(resetText());
+        finishText.text = "Quick, get in the plane and destroy the buildings to collect the relic!"; //instructions text shown when first loading into the level 
+        StartCoroutine(resetText()); //another text reset coroutine
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void BuildingDestroyed()
+    public void BuildingDestroyed() //method that gets called whenever a building is destroyed
     {
         buildingsDestroyed++;
-        if(buildingsDestroyed == buildingsRequired)
+        if(buildingsDestroyed == buildingsRequired)//upon destroying the final one required, the relic is spawned and the player can land
         {
             relic.SetActive(true);
-            plane.collectedRelic = true;
+            plane.collectedRelic = true; //setting the plane controller boolean to true
             Debug.Log("Relic can be collected");
-            finishText.text ="Good job! " + relic.GetComponent<CollectedObject>().objName + " can be collected, press E to land!";
+            finishText.text ="Good job! " + relic.GetComponent<CollectedObject>().objName + " can be collected, press E to land!"; //instructions for the player
             StartCoroutine(resetText());
         }
     }
 
-    IEnumerator resetText()
+    IEnumerator resetText() // the text reset coroutine
     {
         yield return new WaitForSeconds(2.5f);
         finishText.text = "";
